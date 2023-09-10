1e3b0a5990eb5ab3a2270fbf817437f9319cdcc9
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-09-10 18:09:19 -0400

    Completed Exercise 1

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            for (int i = 1; i <= 5; i++)
+            {
+                QuoteGenerator.KanyeQuote();
+
+                for (int j = 1; j <= 1; j++)
+                {
+                    QuoteGenerator.RonQuote();
+                }
+            }
+
+
         }
     }
 }
---
+++ APIsAndJSON/QuoteGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIsAndJSON
+{
+    public class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var kanyeSpeaks = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeSpeaks).GetValue("quote").ToString();
+
+            Console.WriteLine($"----------------------------");
+            Console.WriteLine($"Kanye: \"{kanyeQuote}\"");
+            Console.WriteLine($"----------------------------");
+            Console.WriteLine();
+
+        }
+
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronSpeaks = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronSpeaks).ToString().Replace('[', ' ').Replace(']',' ').Trim();
+
+            Console.WriteLine($"----------------------------");
+            Console.WriteLine($"Ron: {ronQuote}");
+            Console.WriteLine($"----------------------------");
+            Console.WriteLine();
+
+        }
+    }
+}
